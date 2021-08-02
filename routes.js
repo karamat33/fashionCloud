188daac285f24bb627ad10608b0b1f94ad8c101b
karam atrach <karamatrach@hotmail.com>
2021-08-02 16:43:59 +0300

    added unit tests

--- routes.js
+++ routes.js
@@ -6,4 +6,6 @@ module.exports.init= function(app) {
 	app.put('/cache/:key', CacheController.createOrUpdateCache);
 	app.get('/cache/:key', CacheController.readCache);
 	app.get('/cache', CacheController.listCache);
+	app.delete('/cache/:key', CacheController.deleteCache);
+	app.delete('/cache', CacheController.deleteAllCache);
 };
